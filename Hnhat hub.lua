a36ea8fb8ad6481c702d914bb2d6e14390aae818
fire9182882 <143675524+fire9182882@users.noreply.github.com>
2024-01-01 10:16:25 +0700

    Update Hnhat hub.lua

--- Hnhat hub.lua
+++ Hnhat hub.lua
@@ -1,5 +1,5 @@
-local highChestOnly = true
-local godsChalicSniper = false
+highChestOnly = true
+godsChalicSniper = false
 repeat task.wait(4) until game:IsLoaded()
 local PlaceID = game.PlaceId
 local AllIDs = {}
@@ -14,7 +14,7 @@ if not File then
     writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
 end
 function TPReturner()
-    local Site
+    local Site;
     if foundAnything == "" then
         Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
     else
@@ -24,12 +24,12 @@ function TPReturner()
     if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
         foundAnything = Site.nextPageCursor
     end
-    local num = 0
-    for i, v in pairs(Site.data) do
+    local num = 0;
+    for i,v in pairs(Site.data) do
         local Possible = true
         ID = tostring(v.id)
         if tonumber(v.maxPlayers) > tonumber(v.playing) then
-            for _, Existing in pairs(AllIDs) do
+            for _,Existing in pairs(AllIDs) do
                 if num ~= 0 then
                     if ID == tostring(Existing) then
                         Possible = false
@@ -59,12 +59,6 @@ function TPReturner()
     end
 end
 
-function firesignal(signal, ...)
-    if signal and signal.Fire then
-        signal:Fire(...)
-    end
-end
-
 function Teleport()
     while wait() do
         pcall(function()
@@ -75,15 +69,14 @@ function Teleport()
         end)
     end
 end
-
 local veryImportantWaitTime = 0.5
 task.spawn(function()
     while task.wait(veryImportantWaitTime) do
         pcall(function()
-            for i, v in pairs(game.CoreGui:GetDescendants()) do
+            for i,v in pairs(game.CoreGui:GetDescendants()) do
                 pcall(function()
-                    if string.find(v.Name, "ErrorMessage") then
-                        if string.find(v.Text, "Security kick") then
+                    if string.find(v.Name,"ErrorMessage") then
+                        if string.find(v.Text,"Security kick") then
                             veryImportantWaitTime = 1e9
                             Teleport()
                         end
@@ -104,8 +97,8 @@ function AllowRunServiceBind.OnInvoke(args)
     end
     local CoreGui = game:GetService("StarterGui")
     CoreGui:SetCore("SendNotification", {
-        Title = "CHEST Moji",
-        Text = "by Hữu Nhật ",
+        Title = "Chest Moji",
+        Text = "by huunhat",
         Icon = "rbxthumb://type=Asset&id=15485121479&w=150&h=150",
         Duration = math.huge,
         Callback = AllowRunServiceBind,
@@ -113,6 +106,7 @@ function AllowRunServiceBind.OnInvoke(args)
         Button2 = "Tắt",
     })
 end
+
 
 task.spawn(function()
     while task.wait() do
@@ -128,8 +122,8 @@ end)
 
 local CoreGui = game:GetService("StarterGui")
 CoreGui:SetCore("SendNotification", {
-    Title = "CHEST Moji",
-    Text = "by Hữu Nhật",
+    Title = "Chest Moji",
+    Text = "by huunhat",
     Icon = "rbxthumb://type=Asset&id=15485121479&w=150&h=150",
     Duration = math.huge,
     Callback = AllowRunServiceBind,
@@ -153,40 +147,42 @@ task.spawn(function()
             if highChestOnly == false then
                 local hasChar = game.Players.LocalPlayer:FindFirstChild("Character")
                 if not game.Players.LocalPlayer.Character then
+        
                 else
-                    local hasCrewTag = game.Players.LocalPlayer.Character:FindFirstChild("CrewBBG", true)
+                    local hasCrewTag = game.Players.LocalPlayer.Character:FindFirstChild("CrewBBG",true)
                     if hasCrewTag then hasCrewTag:Destroy() end
                     local hasHumanoid = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
                     if hasHumanoid then
                         local Chest = game.Workspace:FindFirstChild("Chest4") or game.Workspace:FindFirstChild("Chest3") or game.Workspace:FindFirstChild("Chest2") or game.Workspace:FindFirstChild("Chest1") or game.Workspace:FindFirstChild("Chest")
-
+                        
                         if Chest then
                             game.Players.LocalPlayer.Character:PivotTo(Chest:GetPivot())
-                            firesignal(Chest.Touched, game.Players.LocalPlayer.Character.HumanoidRootPart)
+                            firesignal(Chest.Touched,game.Players.LocalPlayer.Character.HumanoidRootPart)
                         else
                             Teleport()
                             break
                         end
-                    end
+                    end 
                 end
             elseif highChestOnly == true then
                 local hasChar = game.Players.LocalPlayer:FindFirstChild("Character")
                 if not game.Players.LocalPlayer.Character then
+        
                 else
-                    local hasCrewTag = game.Players.LocalPlayer.Character:FindFirstChild("CrewBBG", true)
+                    local hasCrewTag = game.Players.LocalPlayer.Character:FindFirstChild("CrewBBG",true)
                     if hasCrewTag then hasCrewTag:Destroy() end
                     local hasHumanoid = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
                     if hasHumanoid then
                         local Chest = game.Workspace:FindFirstChild("Chest4") or game.Workspace:FindFirstChild("Chest3") or game.Workspace:FindFirstChild("Chest2")
-
+                        
                         if Chest then
                             game.Players.LocalPlayer.Character:PivotTo(Chest:GetPivot())
-                            firesignal(Chest.Touched, game.Players.LocalPlayer.Character.HumanoidRootPart)
+                            firesignal(Chest.Touched,game.Players.LocalPlayer.Character.HumanoidRootPart)
                         else
                             Teleport()
                             break
                         end
-                    end
+                    end 
                 end
             end
         end
